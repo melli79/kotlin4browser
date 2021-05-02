f738919a958195bbc260d1f397bab5fffcb597a2
melli <melchiorG@gMail.com>
2021-05-03 00:37:23 +0200

    initial setup.

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("js") version "1.4.32"
+}
+
+group = "org.grutzmann.games"
+version = "0.5-SNAPSHOT"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlin/p/kotlin/kotlin-js-wrappers") }
+}
+
+dependencies {
+    testImplementation(kotlin("test-js"))
+    implementation("org.jetbrains:kotlin-react:16.13.1-pre.113-kotlin-1.4.0")
+    implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.113-kotlin-1.4.0")
+    implementation("org.jetbrains:kotlin-styled:1.0.0-pre.113-kotlin-1.4.0")
+}
+
+kotlin {
+    js(LEGACY) {
+        binaries.executable()
+        browser {
+            commonWebpackConfig {
+                cssSupport.enabled = true
+            }
+        }
+    }
+}
